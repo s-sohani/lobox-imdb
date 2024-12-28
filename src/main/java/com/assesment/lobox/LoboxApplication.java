bbe6365529f08dafacb018ce6c8f3281be3a860b
m2s <m.s.sohani73@gmail.com>
2024-12-28 08:34:22 +0330

    init project

---
+++ src/main/java/com/assesment/lobox/LoboxApplication.java
@@ -0,0 +1,13 @@
+package com.assesment.lobox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assesment/lobox/LoboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assesment.lobox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
